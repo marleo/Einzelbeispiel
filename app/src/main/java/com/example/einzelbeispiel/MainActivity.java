9a582fe712b45fc9eb4a16c2de4d7fc3c237e596
Mario Leopold <mariole@edu.aau.at>
2020-03-13 18:14:16 +0100

    Fixed Warnings (Assignment Part 1)

--- app/src/main/java/com/example/einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispiel/MainActivity.java
@@ -3,8 +3,6 @@ package com.example.einzelbeispiel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.renderscript.ScriptGroup;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,7 +13,6 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
-import java.nio.Buffer;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -33,9 +30,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnSubmit = (Button) findViewById(R.id.btnSubmit);
-        submitMatr = (EditText) findViewById(R.id.submitMatr);
-        textServer = (TextView) findViewById(R.id.textServer);
+        btnSubmit = findViewById(R.id.btnSubmit);
+        submitMatr = findViewById(R.id.submitMatr);
+        textServer = findViewById(R.id.textServer);
 
         btnSubmit.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -52,7 +49,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clientTCP() throws InterruptedException {
-        final Handler handler = new Handler();
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -60,25 +56,13 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     String sentence;
 
-                    System.out.println("Started");
-                    BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-                    System.out.println("One more");
                     Socket clientSocket = new Socket("se2-isys.aau.at", 53212);
-                    System.out.println("Connected to Server");
-
                     DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-
                     BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
                     sentence = submitMatr.getText().toString();
-
-                    System.out.println("Your Matr. Nr is: " + sentence);
-
                     outToServer.writeBytes(sentence + '\n');
-
                     response = inFromServer.readLine();
-
-                    System.out.println("Got a response: " + response);
 
                     clientSocket.close();
                 } catch (IOException e) {
